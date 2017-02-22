6fd20e01432e447300db8725ec2b032ed9bf2175
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2017-02-22 10:31:53 +0100

    compartment report improvements: * converter: fail on write errors * readable null:// report for benchmarking * rename null report to dummy since it now also reads * optimize writing * dummy report: optimize read path by not setting random data in the frame by default

--- apps/compartmentConverter.cpp
+++ apps/compartmentConverter.cpp
@@ -47,6 +47,8 @@ using boost::lexical_cast;
         ::exit( EXIT_FAILURE );                         \
     }
 
+namespace
+{
 template< class T > void requireEqualCollections( const T& a, const T& b )
 {
     typename T::const_iterator i = a.begin();
@@ -59,6 +61,19 @@ template< class T > void requireEqualCollections( const T& a, const T& b )
     }
     REQUIRE_EQUAL( i, a.end( ));
     REQUIRE_EQUAL( j, b.end( ));
+}
+
+/** @return true if the cell occupies a continuous region in the report frame */
+bool _isContinuous( const brion::CompartmentReport& report,
+                    const size_t gidIndex )
+{
+    const auto& offsets = report.getOffsets()[gidIndex];
+    const auto& counts = report.getCompartmentCounts()[gidIndex];
+    for( size_t i = 0; i < offsets.size() - 1; ++i )
+        if( offsets[i] + counts[i] != offsets[i+1] )
+            return false;
+    return true;
+}
 }
 
 /**
@@ -90,7 +105,7 @@ int main( const int argc, char** argv )
 #else
         ( "input,i", po::value< std::string >()->required(), "Input report URI")
 #endif
-        ( "output,o", po::value< std::string >()->default_value( "null://" ),
+        ( "output,o", po::value< std::string >()->default_value( "dummy://" ),
           uriHelp.c_str( ))
         ( "erase,e", po::value< std::string >(),
           "Erase the given report (map-based reports only)" )
@@ -193,14 +208,13 @@ int main( const int argc, char** argv )
     clock.reset();
     brion::CompartmentReport to( outURI, brion::MODE_OVERWRITE );
     to.writeHeader( start, end, step, in.getDataUnit(), in.getTimeUnit( ));
-
     {
         size_t index = 0;
         for( const uint32_t gid : gids )
-            to.writeCompartments( gid, counts[ index++ ] );
+            if( !to.writeCompartments( gid, counts[ index++ ] ))
+                return EXIT_FAILURE;
     }
 
-    to.flush(); // write header before parallel section
     float writeTime = clock.getTimef();
 
     const size_t nFrames = (end - start) / step;
@@ -219,12 +233,23 @@ int main( const int argc, char** argv )
         }
 
         const brion::floats& voltages = *data.get();
-        const brion::SectionOffsets& offsets = in.getOffsets();
+        const auto& offsets = in.getOffsets();
 
         size_t index = 0;
         clock.reset();
         for( const uint32_t gid : gids )
         {
+            if( _isContinuous( in, index ))
+            {
+                const float* values = &voltages[ offsets[index][0] ];
+                const size_t size = std::accumulate( counts[index].begin(),
+                                                     counts[index].end(), 0 );
+                if( !to.writeFrame( gid, values, size, t ))
+                    return EXIT_FAILURE;
+                ++index;
+                continue;
+            }
+
             brion::floats cellVoltages;
             cellVoltages.reserve( in.getNumCompartments( index ));
 
@@ -235,7 +260,8 @@ int main( const int argc, char** argv )
                     cellVoltages.emplace_back( voltages[ offset + k ] );
             }
 
-            to.writeFrame( gid, cellVoltages, t );
+            if( !to.writeFrame( gid, cellVoltages, t ))
+                return EXIT_FAILURE;
             ++index;
         }
         writeTime += clock.getTimef();
@@ -248,7 +274,8 @@ int main( const int argc, char** argv )
 
     std::cout << "Converted " << inURI << " to " << outURI
               << " (in " << size_t( loadTime ) << " out " << size_t( writeTime )
-              << " ms)" << std::endl;
+              << " ms, " << gids.size() << " cells X " <<  nFrames << " frames)"
+              << std::endl;
 
     if( vm.count( "compare" ))
     {
