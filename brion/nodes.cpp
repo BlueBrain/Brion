c5934397ebde34a4b59bde543491b05ed907f2fa
Jonas Karlsson <jonaskarlsson@fripost.org>
2018-03-20 15:26:55 +0100

    Parser for the nodes file of the Sonata data format

---
+++ brion/nodes.cpp
@@ -0,0 +1,111 @@
+/* Copyright (c) 2013-2018, EPFL/Blue Brain Project
+ *                          Jonas Karlsson <jonas.karlsson@epfl.ch>
+ *
+ * This file is part of Brion <https://github.com/BlueBrain/Brion>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3.0 as published
+ * by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "nodes.h"
+
+#include <HighFive/include/highfive/H5DataSet.hpp>
+#include <HighFive/include/highfive/H5File.hpp>
+
+#include <iostream>
+#include <memory>
+
+namespace
+{
+static brion::uint32_ts _readIntVector(const HighFive::File& file,
+                                       const std::string& population,
+                                       const std::string& dataset)
+{
+    brion::uint32_ts list;
+    const HighFive::Group group = file.getGroup("/nodes/" + population);
+    const HighFive::DataSet ds = group.getDataSet(dataset);
+    ds.read(list);
+    return list;
+}
+}
+
+namespace brion
+{
+//////////////////////////////////////////////////////////////////////////////
+
+struct Nodes::Impl
+{
+    Impl(const std::string& uri)
+        : file(new HighFive::File(uri))
+    {
+    }
+
+    std::unique_ptr<HighFive::File> file;
+};
+
+Nodes::Nodes(const URI& uri)
+    : impl(new Nodes::Impl(uri.getPath()))
+{
+}
+
+Nodes::~Nodes() = default;
+
+Strings Nodes::getPopulationNames() const
+{
+    Strings populationNames;
+    HighFive::Group group = impl->file->getGroup("/nodes");
+
+    const size_t num_objects = group.getNumberObjects();
+    populationNames.reserve(num_objects);
+
+    for (size_t i = 0; i < num_objects; i++)
+        populationNames.emplace_back(group.getObjectName(i));
+
+    return populationNames;
+}
+
+size_t Nodes::getNumberOfNodes(const std::string population) const
+{
+    return impl->file->getGroup("/nodes/" + population)
+        .getDataSet("node_type_id")
+        .getSpace()
+        .getDimensions()[0];
+}
+
+uint32_ts Nodes::getNodeIDs(const std::string population) const
+{
+    return _readIntVector(*impl->file, population, "node_id");
+}
+
+uint32_ts Nodes::getNodeGroupIDs(const std::string population) const
+{
+    return _readIntVector(*impl->file, population, "node_group_id");
+}
+
+uint32_ts Nodes::getNodeGroupIndices(const std::string population) const
+{
+    return _readIntVector(*impl->file, population, "node_group_index");
+}
+
+uint32_ts Nodes::getNodeTypes(const std::string population) const
+{
+    return _readIntVector(*impl->file, population, "node_type_id");
+}
+
+NodeGroup Nodes::openGroup(const std::string& population, uint32_t groupId)
+{
+    const HighFive::Group group = impl->file->getGroup("/nodes/" + population)
+                                      .getGroup(std::to_string(groupId));
+    return NodeGroup(group);
+}
+}
---
+++ tests/sonata.cpp
@@ -0,0 +1,176 @@
+/* Copyright (c) 2013-2017, EPFL/Blue Brain Project
+ *                          Daniel Nachbaur <daniel.nachbaur@epfl.ch>
+ *                          Juan Hernando <jhernando@fi.upm.es>
+ *
+ * This file is part of Brion <https://github.com/BlueBrain/Brion>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3.0 as published
+ * by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <brain/brain.h>
+#include <brion/brion.h>
+#include <tests/paths.h>
+
+#include <brion/nodes.h>
+#include <brion/types.h>
+
+#include <highfive/H5Utility.hpp>
+
+#define BOOST_TEST_MODULE Sonata
+#include <boost/test/unit_test.hpp>
+#include <cstdarg>
+
+// typedef for brevity
+typedef brion::Vector4f V4f;
+typedef brion::Vector3f V3f;
+
+const brion::URI TEST_SONATA_SIMPLE_NODES_URI(std::string("file://") +
+                                              BRION_TESTDATA +
+                                              "/sonata/simple_nodes.h5");
+
+constexpr char POPULATION_NAME[] = "simple";
+
+BOOST_AUTO_TEST_CASE(sonata_constructors)
+{
+    brion::Nodes nodes0(TEST_SONATA_SIMPLE_NODES_URI);
+
+    HighFive::SilenceHDF5 silence;
+    BOOST_CHECK_THROW(brion::Nodes(brion::URI("void")), std::exception);
+}
+
+BOOST_AUTO_TEST_CASE(sonata_getPopulationNames)
+{
+    brion::Nodes nodes(TEST_SONATA_SIMPLE_NODES_URI);
+    const auto pn = nodes.getPopulationNames();
+    BOOST_CHECK_EQUAL(pn.size(), 1);
+    BOOST_CHECK_EQUAL(pn[0], POPULATION_NAME);
+}
+
+BOOST_AUTO_TEST_CASE(sonata_getNumberOfNodes)
+{
+    brion::Nodes nodes(TEST_SONATA_SIMPLE_NODES_URI);
+    const auto num_nodes = nodes.getNumberOfNodes(POPULATION_NAME);
+    BOOST_CHECK_EQUAL(num_nodes, 20);
+}
+
+BOOST_AUTO_TEST_CASE(sonata_getNodeIDs)
+{
+    brion::Nodes nodes(TEST_SONATA_SIMPLE_NODES_URI);
+    const auto node_ids = nodes.getNodeIDs(POPULATION_NAME);
+    BOOST_CHECK_EQUAL(node_ids.size(), 20);
+    BOOST_CHECK_EQUAL(node_ids[0], 25);
+    BOOST_CHECK_EQUAL(node_ids[10], 69);
+    BOOST_CHECK_EQUAL(node_ids[19], 92);
+}
+
+BOOST_AUTO_TEST_CASE(sonata_getNodeGroupIDs)
+{
+    brion::Nodes nodes(TEST_SONATA_SIMPLE_NODES_URI);
+    const auto node_ids = nodes.getNodeGroupIDs(POPULATION_NAME);
+    BOOST_CHECK_EQUAL(node_ids.size(), 20);
+    BOOST_CHECK_EQUAL(node_ids[0], 0);
+    BOOST_CHECK_EQUAL(node_ids[10], 0);
+    BOOST_CHECK_EQUAL(node_ids[19], 1);
+}
+
+BOOST_AUTO_TEST_CASE(sonata_getNodeGroupIndices)
+{
+    brion::Nodes nodes(TEST_SONATA_SIMPLE_NODES_URI);
+    const auto node_ids = nodes.getNodeGroupIndices(POPULATION_NAME);
+    BOOST_CHECK_EQUAL(node_ids.size(), 20);
+    BOOST_CHECK_EQUAL(node_ids[0], 9);
+    BOOST_CHECK_EQUAL(node_ids[10], 9);
+    BOOST_CHECK_EQUAL(node_ids[19], 4);
+}
+
+BOOST_AUTO_TEST_CASE(sonata_getNodeTypes)
+{
+    brion::Nodes nodes(TEST_SONATA_SIMPLE_NODES_URI);
+    const auto node_ids = nodes.getNodeTypes(POPULATION_NAME);
+    BOOST_CHECK_EQUAL(node_ids.size(), 20);
+    BOOST_CHECK_EQUAL(node_ids[0], 0);
+    BOOST_CHECK_EQUAL(node_ids[10], 2);
+    BOOST_CHECK_EQUAL(node_ids[19], 1);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_CASE(sonata_nodeGroup_getAttributeNames)
+{
+    brion::Nodes nodes(TEST_SONATA_SIMPLE_NODES_URI);
+    auto group = nodes.openGroup(POPULATION_NAME, 0);
+    const auto names = group.getAttributeNames();
+    BOOST_CHECK_EQUAL(names.size(), 8);
+    BOOST_CHECK_EQUAL(names[1], "morphology_file");
+    BOOST_CHECK_EQUAL(names[2], "rotation_angle_x");
+    BOOST_CHECK_EQUAL(names[3], "rotation_angle_y");
+}
+
+BOOST_AUTO_TEST_CASE(sonata_nodeGroup_getDynamicParameterNames)
+{
+    brion::Nodes nodes(TEST_SONATA_SIMPLE_NODES_URI);
+    auto group = nodes.openGroup(POPULATION_NAME, 0);
+    const auto names = group.getDynamicParameterNames();
+    BOOST_CHECK_EQUAL(names.size(), 1);
+    BOOST_CHECK_EQUAL(names[0], "my_dataset");
+}
+
+BOOST_AUTO_TEST_CASE(sonata_nodeGroup_getDynamicParameter)
+{
+    brion::Nodes nodes(TEST_SONATA_SIMPLE_NODES_URI);
+    auto group = nodes.openGroup(POPULATION_NAME, 0);
+
+    const auto my_dataset = group.getDynamicParameter<float>("my_dataset");
+    BOOST_CHECK_EQUAL(my_dataset.size(), 10);
+    BOOST_CHECK_EQUAL(my_dataset[0], 0.1f);
+}
+
+BOOST_AUTO_TEST_CASE(sonata_nodeGroup_getNumberOfNodes)
+{
+    brion::Nodes nodes(TEST_SONATA_SIMPLE_NODES_URI);
+    auto group = nodes.openGroup(POPULATION_NAME, 0);
+    const auto num_nodes = group.getNumberOfNodes();
+    BOOST_CHECK_EQUAL(num_nodes, 8);
+}
+
+BOOST_AUTO_TEST_CASE(sonata_nodeGroup_getAttribute)
+{
+    brion::Nodes nodes(TEST_SONATA_SIMPLE_NODES_URI);
+    auto group = nodes.openGroup(POPULATION_NAME, 0);
+
+    const auto rotation_angle_x = group.getAttribute<float>("rotation_angle_x");
+    const auto rotation_angle_y = group.getAttribute<float>("rotation_angle_y");
+    const auto rotation_angle_z = group.getAttribute<float>("rotation_angle_z");
+    const auto x = group.getAttribute<float>("x", 2, 12);
+    const auto y = group.getAttribute<float>("y", 3, 12);
+    const auto z = group.getAttribute<float>("z", 6, 10);
+
+    BOOST_CHECK_EQUAL(rotation_angle_x.size(), 10);
+    BOOST_CHECK_EQUAL(rotation_angle_x[0], 0.1f);
+
+    BOOST_CHECK_EQUAL(rotation_angle_y.size(), 10);
+    BOOST_CHECK_EQUAL(rotation_angle_y[0], 0.2f);
+
+    BOOST_CHECK_EQUAL(rotation_angle_z.size(), 10);
+    BOOST_CHECK_EQUAL(rotation_angle_z[0], 0.3f);
+
+    BOOST_CHECK_EQUAL(x.size(), 10);
+    BOOST_CHECK_EQUAL(x[0], 2.0f);
+
+    BOOST_CHECK_EQUAL(y.size(), 9);
+    BOOST_CHECK_EQUAL(y[0], 1.0f);
+
+    BOOST_CHECK_EQUAL(z.size(), 4);
+    BOOST_CHECK_EQUAL(z[0], -0.5f);
+}
