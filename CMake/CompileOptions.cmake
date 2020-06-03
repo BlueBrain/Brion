3948c0f96aea9470bd2b711d554553abfc3a33c2
Nadir Román Guerrero <NadirRoGue@users.noreply.github.com>
2020-06-03 15:49:04 +0200

    Attempt to fix Intel compile errors (#285)

--- CMake/CompileOptions.cmake
+++ CMake/CompileOptions.cmake
@@ -4,16 +4,22 @@ function(compile_options TARGET_NAME)
     if(APPLE)
         target_compile_definitions(${TARGET_NAME} PUBLIC Darwin)
     endif()
-    target_compile_options(${TARGET_NAME} PRIVATE -Wnon-virtual-dtor
-                                                  -Wsign-promo
-                                                  -Wvla
-                                                  -fno-strict-aliasing
-                                                  -Wall
-                                                  -Wextra
-                                                  -Winvalid-pch
-                                                  -Winit-self
-                                                  -Wno-unknown-pragmas
-                                                  -Wshadow
-                                                  -Werror
-                                                  -O3)
+    set(COMPILE_OPTIONS)
+    list(APPEND COMPILE_OPTIONS -Wsign-promo
+                                -Wvla
+                                -fno-strict-aliasing
+                                -Wall
+                                -Wextra
+                                -Winvalid-pch
+                                -Winit-self
+                                -Wno-unknown-pragmas
+                                -Wshadow
+                                -Werror
+                                -O3)
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
+        list(APPEND COMPILE_OPTIONS -Wno-non-virtual-dtor -Wno-deprecated)
+    else()
+        list(APPEND COMPILE_OPTIONS -Wnon-virtual-dtor -Wno-deprecated)
+    endif()
+    target_compile_options(${TARGET_NAME} PRIVATE ${COMPILE_OPTIONS})
 endfunction()
